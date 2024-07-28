275b53f38480e5376d18e69e348cbd4664dff53a
Jai <jaisarki123@gmail.com>
2024-07-28 16:34:44 +0530

    handled conditional styling

--- components/LeftPanel.vue
+++ components/LeftPanel.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="bg-gray-700 p-6 rounded-lg">
+  <div
+    :class="{
+      'bg-slate-300	 text-black p-6 rounded-lg': mode === 'light',
+      'bg-gray-800 text-white p-6 rounded-lg': mode === 'dark',
+    }"
+  >
     <div>
-      <Icon name="uil:github" style="color: white; font-size: 48px" />
+      <Icon
+        name="uil:github"
+        :style="{
+          color: mode === 'light' ? 'black' : 'white',
+          fontSize: '48px',
+        }"
+      />
     </div>
     <div class="mt-12">
       <div
@@ -9,7 +20,13 @@
         :key="item.iconName"
         class="flex items-center space-x-2 my-2"
       >
-        <Icon :name="item.iconName" style="color: white; font-size: 24px" />
+        <Icon
+          :name="item.iconName"
+          :style="{
+            color: mode === 'light' ? 'black' : 'white',
+            fontSize: '24px',
+          }"
+        />
         <span class="text-lg font-medium">{{ item.name }}</span>
       </div>
     </div>
@@ -17,24 +34,31 @@
       <hr />
       <div class="flex justify-between">
         <div class="flex items-center">
-          <Icon name="material-symbols:question-mark" />
+          <Icon
+            name="material-symbols:question-mark"
+            :style="{ color: mode === 'light' ? 'black' : 'white' }"
+          />
           <p>Help & getting started</p>
         </div>
-        <Icon name="material-symbols:shield-person-rounded" />
+        <Icon
+          name="material-symbols:shield-person-rounded"
+          :style="{ color: mode === 'light' ? 'black' : 'white' }"
+        />
       </div>
       <div class="bg-black rounded-full">
         <div class="flex">
           <button
             class="flex items-center justify-center basis-3/6 gap-x-2 p-3"
+            @click="setMode('light')"
           >
             <Icon name="material-symbols:sunny" style="color: white" />
             <span class="text-white">Light</span>
           </button>
           <button
             class="flex items-center justify-center basis-3/6 gap-x-2 p-3"
+            @click="setMode('dark')"
           >
             <Icon name="material-symbols:mode-night" style="color: white" />
-
             <span class="text-white">Dark</span>
           </button>
         </div>
@@ -44,11 +68,21 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
 import { leftPanelDummydata } from "../utils/dummyData";
 import { useUserStore } from "../stores/users";
+
+const mode = ref("");
 const userStore = useUserStore();
 
+const setMode = (newMode) => {
+  mode.value = newMode;
+  localStorage.setItem("userSelectedPreferedMode", newMode);
+  userStore.setuserSelectedPreferedMode(newMode);
+};
+
 onMounted(() => {
-  userStore.setuserSelectedPreferedMode("dark");
+  mode.value = localStorage.getItem("userSelectedPreferedMode") || "dark";
+  userStore.setuserSelectedPreferedMode(mode.value);
 });
 </script>
