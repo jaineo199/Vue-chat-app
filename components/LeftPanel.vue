25f60fb8cacee005679d07d570ec87c8f8588aea
Jai <jaisarki123@gmail.com>
2024-07-28 12:08:23 +0530

    added static data in left panel

--- components/LeftPanel.vue
+++ components/LeftPanel.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <Icon name="uil:github" style="color: black; font-size: 48px" />
+    <div
+      v-for="item in leftPanelDummydata"
+      :key="item.iconName"
+      class="flex items-center space-x-2 my-2"
+    >
+      <Icon :name="item.iconName" style="color: black; font-size: 24px" />
+      <span class="text-lg font-medium">{{ item.name }}</span>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { leftPanelDummydata } from "../utils/dummyData";
+</script>
---
+++ utils/dummyData.ts
@@ -0,0 +1,22 @@
+export const leftPanelDummydata = [
+  {
+    iconName: "material-symbols:house",
+    name: "Home",
+  },
+  {
+    iconName: "material-symbols:production-quantity-limits",
+    name: "Products",
+  },
+  {
+    iconName: "material-symbols:inbox-customize-sharp",
+    name: "Customers",
+  },
+  {
+    iconName: "material-symbols:add-shopping-cart",
+    name: "Shop",
+  },
+  {
+    iconName: "material-symbols:align-vertical-bottom-rounded",
+    name: "Income",
+  },
+];
